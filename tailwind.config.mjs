bc9fe4a3014cafd3ec7b9ae04e696bf5bd1ca3f9
Sergio <sergio.pm.developer@gmail.com>
2024-05-29 23:21:36 +0000

    style: add theme colors and small screen

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,15 @@
 export default {
   content: ["./src/**/*.astro"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "theme-green": "#98C173",
+        "theme-red": "#DA2528",
+        "theme-black": "#000000",
+      },
+      screens: {
+        xs: "480px",
+      },
+    },
   },
-  plugins: [],
 };
